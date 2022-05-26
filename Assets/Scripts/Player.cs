5090292eba3aa243dc8f849f2c78bcd02a25296c
Nopsy11 <91734682+Nopsy11@users.noreply.github.com>
2022-05-27 00:33:58 +0200

    suelo lvl1 acabado

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,32 +13,47 @@ public class Player : MonoBehaviour {
 
     // Update is called once per frame
     void Update()    {
+
+        // sprint izquierda
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.LeftShift)) {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-2000f * Time.deltaTime, 0));
+            gameObject.GetComponent<Animator>().SetBool("moving", true);
+            gameObject.GetComponent<SpriteRenderer>().flipX =  true;
+        }
+
+        //sprint derecha
+        if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.LeftShift)) {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(2000f * Time.deltaTime, 0));
+            gameObject.GetComponent<Animator>().SetBool("moving", true);
+            gameObject.GetComponent<SpriteRenderer>().flipX =  false;
+        }
+
         //izquierda
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+        if (Input.GetKey(KeyCode.A)) {
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-1500f * Time.deltaTime, 0));
             gameObject.GetComponent<Animator>().SetBool("moving", true);
             gameObject.GetComponent<SpriteRenderer>().flipX =  true;
         }
         
         //derecha
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+        if (Input.GetKey(KeyCode.D)) {
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(1500f * Time.deltaTime, 0));
             gameObject.GetComponent<Animator>().SetBool("moving", true);
             gameObject.GetComponent<SpriteRenderer>().flipX =  false;
         }
 
         //saltar
-        if (Input.GetKeyDown(KeyCode.W) && puedeSaltar || Input.GetKeyDown(KeyCode.UpArrow) && puedeSaltar) {
+        if (Input.GetKeyDown(KeyCode.Space) && puedeSaltar) {
             puedeSaltar = false;
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 1000f));
         }
 
         //comprobar si se mueve
-        if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.RightArrow)){
+        if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D)){
             gameObject.GetComponent<Animator>().SetBool("moving", false);
         }
 
-        // if(Input.GetKeyDown(KeyCode.Space)) {
+        // if(Input.GetKeyDown(KeyCode.J)) {
         //     Atacar();
         // }
     }
